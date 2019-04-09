80182e29d96cbc610298583f846088a327369ca6
fuhaiyang <fuhaiyang@xycentury.com>
2019-04-09 19:07:44 +0800

    update

--- Example/BSBase/ViewController.swift
+++ Example/BSBase/ViewController.swift
@@ -8,17 +8,24 @@
 
 import UIKit
 import Alamofire
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+//    路由器与请求适配器有什么逻辑？
+//    简单的静态数据（如路径，HTTP方法和公共标头）属于Router。动态数据，例如Authorization其值可以基于认证系统而改变的标题属于a RequestAdapter。
+    
+
+    open var mysessionManager :SessionManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        urlEncoding()
 //        jsonEncoding()
-        
-        dataUpload()
-        
-        
+//        dataUpload()
+//        sessionManagertest()
+//        requestConfig()
+//        myRouter()
+        myAdapter()
     }
 }
 
@@ -110,4 +117,107 @@ extension ViewController {
         })
     }
     
+    
+    
+    
+    @objc public func sessionManagertest() {
+        var defaultHeaders = SessionManager.defaultHTTPHeaders
+        defaultHeaders["DNT"] = "1 (Do Not Track Enabled)"
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = defaultHeaders
+        
+        mysessionManager = SessionManager(configuration: configuration)
+        mysessionManager!.request("https://httpbin.org/get").responseJSON { Response in
+            print(Response)
+        }
+    }
+    
+    
+    @objc public func requestConfig(){
+        request(BSHttpURL("dad")).response { (DataResponse) in
+            print(DataResponse)
+        }
+    }
+    
+    @objc public func myRouter(){
+        request(Router.search(query: "foo bar", page: 1))
+    }
+    
+    
+    @objc public func myAdapter(){
+//        mysessionManager = SessionManager.default;
+//        mysessionManager?.adapter = AccessTokenAdapter(accessToken: "1234")
+//        mysessionManager?.request("https://httpbin.org/get")
+        Alamofire.request("https://example.com/users/mattt").responseJSON { (response: DataResponse<Any>) in
+            let userResponse = response.map { json in
+                // We assume an existing User(json: Any) initializer
+                return User(json: json)
+            }
+            
+            // Process userResponse, of type DataResponse<User>:
+            if let user = userResponse.value {
+                print("User: { username: \(user.username), name: \(user.name) }")
+            }
+        }
+    }
+    
+}
+
+
+class BSHttpURL: NSObject,URLConvertible {
+    static let baseURLString = "https://example.com"
+    var urlname:String?
+    
+    init(_ name:String) {
+        urlname = name
+    }
+    func asURL() throws -> URL {
+        let urlString = BSHttpURL.baseURLString + "/users/\(urlname!)"
+        return try urlString.asURL()
+    }
+}
+
+enum Router: URLRequestConvertible {
+    case search(query: String, page: Int)
+    
+    static let baseURLString = "https://example.com"
+    static let perPage = 50
+    
+    // MARK: URLRequestConvertible
+    
+    func asURLRequest() throws -> URLRequest {
+        let result: (path: String, parameters: Parameters) = {
+            switch self {
+            case let .search(query, page) where page > 0:
+                return ("/search", ["q": query, "offset": Router.perPage * page])
+            case let .search(query, _):
+                return ("/search", ["q": query])
+            }
+        }()
+        
+        let url = try Router.baseURLString.asURL()
+        let urlRequest = URLRequest(url: url.appendingPathComponent(result.path))
+        
+        return try URLEncoding.default.encode(urlRequest, with: result.parameters)
+    }
+}
+
+
+class AccessTokenAdapter: RequestAdapter {
+    private let accessToken: String
+    
+    init(accessToken: String) {
+        self.accessToken = accessToken
+    }
+    
+    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+        var urlRequest = urlRequest
+        
+        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix("https://httpbin.org") {
+            urlRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+        }
+        
+        return urlRequest
+    }
 }
